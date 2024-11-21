db5dbe886f9830742f46310706c5d0b084dc8b2f
Tom Cumming <mail@tomcumming.co.uk>
2024-11-21 21:59:17 +0000

    Add dev flake

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  };
+  outputs = { self, nixpkgs }:
+    let
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages."${system}";
+    in
+    {
+      devShells."${system}".default = pkgs.mkShell {
+        packages = [
+          pkgs.nodejs_22
+        ];
+      };
+      formatter."${system}" = pkgs.nixpkgs-fmt;
+    };
+}
